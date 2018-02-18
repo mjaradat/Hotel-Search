8d35351f8e2cb7589350962ba3c66050464495b1
mjaradat <tjaradat1@gmail.com>
2018-02-18 14:22:42 +0200

    Update errors.js

--- common/errors.js
+++ common/errors.js
@@ -3,6 +3,6 @@ module.exports = {
     2: "The check in date cannot be empty",
     3: "The check out date cannot be empty",
     4: "The check out date cannot be older than check in date",
-    5: "The max guest raing should be between 1.0 to 5.0",
-    6: "The max total raing should be between 1.0 to 5.0"
+    5: "The max guest rating should be between 1.0 to 5.0",
+    6: "The max star rating should be between 1.0 to 5.0"
 };
